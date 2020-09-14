fa2de80dd133b5a078af6fa94b828ee072a64179
Muddyweavy <54936267+Muddyweavy@users.noreply.github.com>
2020-09-14 15:29:49 +0200

    Add files via upload

---
+++ H03/3.8.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>h3 opdracht 7</title>
+    <style>
+
+    </style>
+  </head>
+  <body>
+
+    <?php
+
+   ?>
+
+  </body>
+</html>
