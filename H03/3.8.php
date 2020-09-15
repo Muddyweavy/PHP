e4510946499a35a3adc570ecfc4967c6dbd8db66
Muddyweavy <54936267+Muddyweavy@users.noreply.github.com>
2020-09-15 15:41:41 +0200

    Add files via upload

--- H03/3.8.php
+++ H03/3.8.php
@@ -2,7 +2,7 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>h3 opdracht 7</title>
+    <title>h3 opdracht 8</title>
     <style>
 
     </style>
@@ -11,6 +11,21 @@
 
     <?php
 
+    $kappersagenda = [
+      "9:15" => "mevr Pietersen",
+      "9:30" => "mevr willems",
+      "9:45" => "",
+      "10:00" => "paul van den broek",
+      "10:15" => "Karel de meeuw",
+      "10:30" => ""
+    ];
+
+    print("de volgende momenten zijn nog beschikbaar:<ul>");
+    foreach($kappersagenda as $tijd => $afspraak) {
+      if ($afspraak == "") {
+        print("<li>".$tijd."</li>") ;
+      }
+    }
    ?>
 
   </body>
